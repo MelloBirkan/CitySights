282f9ea09a955944eac20ba6a41d8ef122aa7388
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-02-25 14:19:24 -0300

    Add API key configuration and update search button UI

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -13,12 +13,22 @@ struct ContentView: View {
     HStack {
       TextField("What're you looking for?", text: $query)
         .textFieldStyle(.roundedBorder)
-      
-      Button("Go") {
+
+      Button {
         // TODO: implemente query
+      } label: {
+        Image(systemName: "magnifyingglass")
+          .foregroundColor(.white)
+          .padding(10)
+          .background(Color.blue)
+          .cornerRadius(10)
+          .shadow(radius: 2)
       }
     }
     .padding()
+    .onAppear {
+      print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+    }
   }
 }
 
