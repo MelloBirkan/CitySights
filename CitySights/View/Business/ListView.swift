a063e062adba1653f941e74066a6cc67179408c0
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-03-13 15:02:15 -0300

    Add BusinessDetailView, ListView, MapView, and OnboardingView for enhanced user experience and navigation

--- CitySights/View/Business/ListView.swift
+++ CitySights/View/Business/ListView.swift
@@ -9,25 +9,39 @@ import SwiftUI
 
 struct ListView: View {
     @Environment(BusinessModel.self) private var businessModel
-
+    
     var body: some View {
         ZStack {
             Color(.systemBackground)
-
+            
             List {
                 ForEach(businessModel.businesses) { business in
                     HStack {
-                        Image(.foodPlaceholder)
+                        if let imageUrl = business.imageUrl {
+                            AsyncImage(url: URL(string: imageUrl)) { Image in
+                                Image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 50, height: 50)
+                                    .clipShape(.rect(cornerRadius: 12))
+                            } placeholder: {
+                                ProgressView()
+                                    .frame(width: 60, height: 60)
+                            }
+                            
+                        } else {
+                            Image(.foodPlaceholder)
+                        }
                         VStack(alignment: .leading) {
                             Text(business.name ?? "Restaurant Name")
                                 .font(.headline)
-
+                            
                             Text(calculateTimeInMinutes(distance: business.distance))
                                 .foregroundStyle(.secondary)
                                 .font(.caption2)
                         }
                         Spacer()
-
+                        
                         Image("regular_\(round(business.rating ?? 0))")
                     }
                     .onTapGesture {
@@ -40,7 +54,7 @@ struct ListView: View {
         }
         .clipShape(.rect(cornerRadius: 12))
     }
-
+    
     // Função para converter distância em metros para tempo em minutos
     // Assumindo velocidade de caminhada média de 5km/h ou aproximadamente 83 metros por minuto
     func calculateTimeInMinutes(distance: Double?) -> String {
@@ -49,7 +63,7 @@ struct ListView: View {
         }
         let walkingSpeedMetersPerMinute: Double = 83
         let timeInMinutes = distance / walkingSpeedMetersPerMinute
-
+        
         // Formato para exibir apenas um decimal
         return String(format: "%.0f min away walking", timeInMinutes)
     }
